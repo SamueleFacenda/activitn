9b9111870e75a0f4c057c441dd176d18622a6a38
SamueleFacenda <samuele.facenda@gmail.com>
2024-12-05 14:31:25 +0100

    feat: add grommet to frontend for responsive design
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useContext } from "react";
+import { 
+  Button, 
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Grid,
+  Grommet, 
+  grommet,
+  Header, 
+  Heading,
+  Page, 
+  PageContent, 
+  PageHeader, 
+  Paragraph,
+  ResponsiveContext,
+  Text 
+} from 'grommet';
+import { deepMerge } from "grommet/utils";
+import { Moon, Sun } from "grommet-icons";
+
+const theme = deepMerge(grommet, {
+  global: {
+    colors: {
+      brand: '#228BE6',
+    },
+    font: {
+      family: "Roboto",
+      size: "18px",
+      height: "20px",
+    },
+  },
+});
+
+const AppBar = (props) => (
+  <Header
+    background="brand"
+    pad={{ left: "medium", right: "small", vertical: "small" }}
+    elevation="medium"
+    {...props}
+  />
+);
+
+const CardTemplate = ({ title }) => {
+  const size = useContext(ResponsiveContext);
+  return (
+    <Card>
+      <CardHeader pad="medium">
+        <Heading level={2} margin="none">
+          {title}
+        </Heading>
+      </CardHeader>
+      <CardBody pad="medium">
+        <Paragraph maxLines={size === "small" ? 3 : undefined}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+          porttitor non nulla ac vehicula. Aliquam erat volutpat. Mauris auctor
+          faucibus est at mattis. Aliquam a enim ac nisi aliquam consectetur et
+          ac velit. Mauris ut imperdiet libero.
+        </Paragraph>
+      </CardBody>
+      <CardFooter pad="medium" background="background-contrast">
+        Footer
+      </CardFooter>
+    </Card>
+  );
+};
+  
 
 function App() {
+  const [dark, setDark] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grommet theme={theme} full themeMode={dark ? "dark" : "light"}>
+      <Page>
+
+        <AppBar>
+          <Text size="large">Activitn</Text>
+          <Button
+            a11yTitle={dark ? "Switch to Light Mode" : "Switch to Dark Mode"}
+            icon={dark ? <Moon /> : <Sun />}
+            onClick={() => setDark(!dark)}
+          />
+        </AppBar>
+        <PageContent>
+          <PageHeader title="Welcome to Grommet!" />
+          <Grid columns="medium" gap="large" pad={{ bottom: "large" }}>
+            <CardTemplate title={"Card 1"} />
+            <CardTemplate title={"Card 2"} />
+            <CardTemplate title={"Card 3"} />
+          </Grid>
+        </PageContent>
+      </Page>
+    </Grommet>
   );
 }
 
