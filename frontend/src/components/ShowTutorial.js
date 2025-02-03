93948d75e6d042f2c9e67e438821eafac59fe9b0
Yifeizh04 <yifeizh04@gmail.com>
2025-02-03 21:36:38 +0100

    feat: Add ShowTutorial component

---
+++ frontend/src/components/ShowTutorial.js
@@ -0,0 +1,34 @@
+import { Button, Box, Text } from "grommet";
+
+import { Catalog } from "grommet-icons";
+
+import { useState } from "react";
+
+/**
+ * ShowTutorial component that renders a button to show a tutorial text
+ * @param {String} tutorial - text of the tutorial
+ * @returns {JSX.Element} - ShowTutorial component
+ */
+const ShowTutorial = ({ tutorial }) => {
+  const [showTutorial, setShowTutorial] = useState(false);
+  return (
+    <Box>
+      <Button
+        icon={<Catalog />}
+        label="Show Tutorial"
+        onClick={() => setShowTutorial(!showTutorial)}
+        primary
+      />
+      {showTutorial && (
+        <Box
+          animation="slideIn"
+          style={{ transition: "transform 0.3s ease" }}
+        >
+          <Text>{tutorial}</Text>
+        </Box>
+      )}
+    </Box>
+  );
+};
+
+export { ShowTutorial };
