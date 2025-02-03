4c14cb1dcf6d6a88da4ef907d1f02d07075bb0c0
Yifeizh04 <yifeizh04@gmail.com>
2025-02-03 21:33:33 +0100

    feat: Add the ViewButton component

---
+++ frontend/src/components/ViewButton.js
@@ -0,0 +1,21 @@
+import { RoutedButton } from "./RoutedButton";
+
+import { View } from "grommet-icons";
+
+/**
+ * ViewButton component that renders a button to view an event
+ * @param {Object} event - event object
+ * @returns {JSX.Element} - ViewButton component
+ */
+const PATH = "/events";
+const ViewButton = ({event}) => {
+  return (
+    <RoutedButton
+      path={`${PATH}/${event._id}`}
+      label="View"
+      icon={<View />}
+    />
+  );
+}
+
+export { ViewButton };
