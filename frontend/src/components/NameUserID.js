996ab128b42ea7b8982090f2d092e83e56a3266d
Yifeizh04 <yifeizh04@gmail.com>
2025-02-03 21:40:10 +0100

    feat: Add NameUserId.js

---
+++ frontend/src/components/NameUserID.js
@@ -0,0 +1,48 @@
+import { useGetUsersById } from "../api/queries";
+
+import { Text, Spinner } from "grommet";
+
+import { useAuth } from "../hooks/Auth";
+
+const NAME_MAX_LENGTH = 20;
+// Method to truncate the name if it is too long
+const truncateNameLength = (name) => {
+  if (name.length > NAME_MAX_LENGTH) {
+    return name.slice(0, NAME_MAX_LENGTH) + "...";
+  }
+  return name;
+};
+
+/**
+ * A component that displays the name of the user with the given ID
+ * @param {id} String - The user ID 
+ * @returns {Text} - The name of the user with the given ID
+ */
+const NameUserID = ({ id }) => {
+  console.log("ID: ", id);
+  const { state } = useAuth();
+  const { isAuthenticated } = state;
+  const { data, isPending, isError, error } = useGetUsersById(
+    { id: id },
+    undefined,
+    {
+      enabled: isAuthenticated,
+    }
+  );
+  console.log("Data: ", data);
+  console.log("IsPending: ", isPending);
+  console.log("isAuthenticated: ", isAuthenticated);
+  if (isPending) {
+    return <Spinner />;
+  }
+
+  if (isError) {
+    return <Text>Error: {error.message}</Text>;
+  }
+  
+  const user = isPending ? [] : data;
+  console.log("User: ", user);
+  return <Text>{truncateNameLength(user.name)}</Text>;
+};
+
+export { NameUserID };
