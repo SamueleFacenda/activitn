4498896d1350f457e49619283a0745be64669358
Yifeizh04 <yifeizh04@gmail.com>
2025-02-04 13:29:56 +0100

    feat: Add buttons as props and improve the style

--- frontend/src/components/EventCard.js
+++ frontend/src/components/EventCard.js
@@ -1,23 +1,48 @@
-import { Card, CardHeader, CardBody, Spinner, Heading, Text, Icon, Grommet } from "grommet";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Spinner,
+  Heading,
+  Text,
+  Box,
+} from "grommet";
 import styled from "styled-components";
 
-import {
-  Group,
-  Location,
-  Calendar,
-  Notes,
-} from "grommet-icons";
+import { Group, Location, Calendar, Notes, Tag } from "grommet-icons";
 
 // maybe replace the icons with: ✍️👥📍📅
-
 const StyledCard = styled(Card)`
+  width: max-content;
   transition: all 0.3s ease;
   &:hover {
-    transform: scale(1.2);
+    transform: scale(1.1);
   }
 `;
 
-export const EventCard = ({ event, isLoading }) => {
+// function to convert the date to the European format
+function convertToEuropeanFormat(dateString) {
+  const date = new Date(dateString);
+
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const year = date.getFullYear();
+
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+
+  return `${day}-${month}-${year} - ${hours}:${minutes}`;
+}
+
+/**
+ * EventCard component that represents a card for an event
+ * @param {props} param0 - event, isLoading, buttons
+ * @param {Object} event - event
+ * @param {boolean} isLoading - loading state
+ * @param {Array} buttons - array of buttons (The buttons has onClick method that receives the event as parameter)
+ * @returns {JSX.Element} - EventCard component
+ */
+export const EventCard = ({ event, isLoading, buttons }) => {
   if (isLoading) {
     return (
       <Card
@@ -43,11 +68,6 @@ export const EventCard = ({ event, isLoading }) => {
     return description;
   };
 
-  const onClick = (id) => {
-    // TODO: Navigate to event page
-    console.log("Clicked");
-  };
-
   return (
     <StyledCard
       background="background-contrast"
@@ -55,26 +75,40 @@ export const EventCard = ({ event, isLoading }) => {
       elevation="small"
       round="small"
       pad="medium"
-      width="medium"
-      onClick={() => onClick()}
+      // width="min-content"
     >
       <CardHeader>
-          <Heading level="3" margin="none">
-            {event.name}
-          </Heading>
-        </CardHeader>
-        <CardBody>
-          <Text size="small">
-            <Calendar /> <strong>Data:</strong> {new Date(event.date).toISOString()}
-          </Text>
-          <Text size="small"> <Location color="red"/> {event.location}</Text>
-          <Text size="small"><Group color="blue" /> {event.group} partecipanti</Text>
-          <Text size="small" margin={{ top: "small" }}>
-            <Notes />
-            <strong>Descrizione:</strong> {truncateDescription(event.description)}
-          </Text>
-        </CardBody>
-      
+        <Heading level="3" margin="none">
+          {event.name}
+        </Heading>
+      </CardHeader>
+      <CardBody>
+        <Text size="small">
+          <Calendar /> <strong>Data:</strong>{" "}
+          {convertToEuropeanFormat(event.date)}
+        </Text>
+        <Text size="small">
+          <Location color="red" /> {event.location}
+        </Text>
+        {/* <Text size="small">
+          <Group color="blue" /> {event.group} partecipanti
+        </Text> */}
+        <Text size="small">
+          <Tag /> {event.tag}
+        </Text>
+        <Text size="small" margin={{ top: "small" }}>
+          <Notes />
+          <strong>Descrizione:</strong> {truncateDescription(event.description)}
+        </Text>
+        <Box direction="row" gap="small" margin={{ top: "small" }} wrap={true}>
+          {
+            /* array of buttons */
+            buttons.map((Btn) => (
+              <Btn event={event} />
+            ))
+          }
+        </Box>
+      </CardBody>
     </StyledCard>
   );
 };
