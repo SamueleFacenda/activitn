8c9194bc7d876f291ffe16e9a80699f67c3508f8
Yifeizh04 <yifeizh04@gmail.com>
2025-02-04 19:57:58 +0100

    fix: Remove the page Events and update the export of the pages

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,5 +1,8 @@
 
 export * from './Home';
 export * from './UserSettings';
-export * from './Events';
 export * from './Login';
+export * from './SearchEvent';
+export * from './MyEvents';
+export * from './CreateEvent';
+export * from './ModifyEvent';
