f279569719f57ab388a23ecb12981283a16a09bd
Yifeizh04 <yifeizh04@gmail.com>
2025-02-04 19:57:06 +0100

    feat: Create the page MyEvents

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -6,11 +6,20 @@ import {
 
 import { EventsList, FormEvent } from '../components';
 
+const event = {
+    name: "Event",
+    description: "Description",
+    date: "Date",
+    location: "Location",
+    group: "Group"
+}
+
 const Events = () => (
     <PageContent flex="grow">       
         <PageHeader title="Events"/>
-        <FormEvent/>
-        <EventsList/>
+        {/* <FormEvent/> */}
+        {/* <EventsList/> */}
+        {/* <EventDescription event={event}/> */}
     </PageContent>
 )
 
